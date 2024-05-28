5e1a833b3cb6e639fa7539e6560940ff3002a0e9
Lizaza <lazarenkolizka@yandex.ru>
2024-05-28 08:55:27 +0300

    update markets, update main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import datetime
+import pprint
+from time import sleep
 
 from utils import csv_utils
+from web import web_utils
+from currency.currency import Currency
 from threading import Thread
 import currency.static as static
 
@@ -10,11 +14,11 @@ second_curr = "usdt"
 
 
 def main():
-    # Создаем файл с текущей датой
+    # Создаем название для файла с текущей датой
     file_name = datetime.datetime.now().strftime("%m.%d.%Y %H-%M-%S")
     # Создаем потоки для каждой валюты
     for curr in static.currencies:
-        threads.append(Thread())
+        threads.append(Thread(target=web_utils.parse_currency, args=(curr, second_curr, currencies)))
     # Начинаем потоки
     for t in threads:
         t.start()
